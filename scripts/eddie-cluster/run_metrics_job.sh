aca0d80132e1024647a429d7f45f9df271e5df34
hallaemiia <hallaemilia@gmail.com>
2026-07-07 19:39:22 +0100

    Point metric jobs at scratch checkpoints

--- scripts/eddie-cluster/run_metrics_job.sh
+++ scripts/eddie-cluster/run_metrics_job.sh
@@ -19,22 +19,47 @@ CONFIG_PATH="${CONFIG_PATH:?Set CONFIG_PATH.}"
 CLASSIFIER_CONFIG="${CLASSIFIER_CONFIG:?Set CLASSIFIER_CONFIG.}"
 MODEL_NAME="${MODEL_NAME:?Set MODEL_NAME.}"
 DATASET_LABEL="${DATASET_LABEL:?Set DATASET_LABEL.}"
+CHECKPOINT_STAGE="${CHECKPOINT_STAGE:-train}"
+CHECKPOINT_DIR="${CHECKPOINT_DIR:-$EDDIE_SCRATCH_ROOT/checkpoints/$DATASET_LABEL/$MODEL_NAME/$CHECKPOINT_STAGE}"
+CLASSIFIER_CKPT_DIR="${CLASSIFIER_CKPT_DIR:-}"
 NUM_SAMPLES="${NUM_SAMPLES:-1000}"
 CG_SAMPLES="${CG_SAMPLES:-500}"
 BATCH_SIZE="${BATCH_SIZE:-128}"
 MAX_DIMS_PER_LAYER="${MAX_DIMS_PER_LAYER:-}"
+RUN_CONFIG_DIR="$EDDIE_SCRATCH_ROOT/run_configs/metrics/$DATASET_LABEL/$MODEL_NAME"
 
 # shellcheck disable=SC1090
 source "$VENV_DIR/bin/activate"
 
 export PYTHONPATH="$PROJECT_DIR/counterfactual_benchmark:$PROJECT_DIR:${PYTHONPATH:-}"
 OUT_DIR="$EDDIE_SCRATCH_ROOT/results/raw"
-mkdir -p "$OUT_DIR"
+mkdir -p "$OUT_DIR" "$RUN_CONFIG_DIR"
+
+RUNTIME_CONFIG="$RUN_CONFIG_DIR/model_config.json"
+RUNTIME_CLASSIFIER_CONFIG="$RUN_CONFIG_DIR/classifier_config.json"
+python - "$PROJECT_DIR/$CONFIG_PATH" "$RUNTIME_CONFIG" "$CHECKPOINT_DIR" "$PROJECT_DIR/$CLASSIFIER_CONFIG" "$RUNTIME_CLASSIFIER_CONFIG" "$CLASSIFIER_CKPT_DIR" <<'PY'
+import json
+import sys
+
+model_source, model_target, checkpoint_dir, clf_source, clf_target, classifier_ckpt_dir = sys.argv[1:7]
+with open(model_source, "r", encoding="utf-8") as handle:
+    model_config = json.load(handle)
+model_config["checkpoint_dir"] = checkpoint_dir
+with open(model_target, "w", encoding="utf-8") as handle:
+    json.dump(model_config, handle, indent=2)
+
+with open(clf_source, "r", encoding="utf-8") as handle:
+    clf_config = json.load(handle)
+if classifier_ckpt_dir:
+    clf_config["ckpt_path"] = classifier_ckpt_dir.rstrip("/") + "/"
+with open(clf_target, "w", encoding="utf-8") as handle:
+    json.dump(clf_config, handle, indent=2)
+PY
 
 cmd=(
   python -m causal_disentanglement_metrics.run_metrics
-  --config "$PROJECT_DIR/$CONFIG_PATH"
-  --classifier-config "$PROJECT_DIR/$CLASSIFIER_CONFIG"
+  --config "$RUNTIME_CONFIG"
+  --classifier-config "$RUNTIME_CLASSIFIER_CONFIG"
   --model-name "$MODEL_NAME"
   --dataset-name "$DATASET_LABEL"
   --num-samples "$NUM_SAMPLES"
--- scripts/eddie-cluster/submit_metrics_matrix.sh
+++ scripts/eddie-cluster/submit_metrics_matrix.sh
@@ -16,21 +16,22 @@ submit_metrics() {
   local h_rt="$5"
   local h_rss="$6"
   local batch_size="${7:-64}"
+  local checkpoint_stage="${8:-train}"
   local job_name="cfb-metrics-${dataset_label}-${model_name}"
   qsub -N "$job_name" -q gpu -l gpu=1 -pe sharedmem 4 -l h_rt="$h_rt" -l h_rss="$h_rss" \
     -o "$LOG_ROOT/${job_name}.out" -e "$LOG_ROOT/${job_name}.err" \
-    -v PROJECT_DIR="$PROJECT_DIR",EDDIE_SCRATCH_ROOT="$EDDIE_SCRATCH_ROOT",DATASET_LABEL="$dataset_label",MODEL_NAME="$model_name",CONFIG_PATH="$config_path",CLASSIFIER_CONFIG="$classifier_config",BATCH_SIZE="$batch_size" \
+    -v PROJECT_DIR="$PROJECT_DIR",EDDIE_SCRATCH_ROOT="$EDDIE_SCRATCH_ROOT",DATASET_LABEL="$dataset_label",MODEL_NAME="$model_name",CONFIG_PATH="$config_path",CLASSIFIER_CONFIG="$classifier_config",BATCH_SIZE="$batch_size",CHECKPOINT_STAGE="$checkpoint_stage" \
     "$SCRIPT"
 }
 
 submit_metrics morphomnist vae counterfactual_benchmark/methods/deepscm/configs/morphomnist/vae.json counterfactual_benchmark/methods/deepscm/configs/morphomnist/classifier.json 12:00:00 16G 128
-submit_metrics morphomnist hvae counterfactual_benchmark/methods/deepscm/configs/morphomnist/hvae.json counterfactual_benchmark/methods/deepscm/configs/morphomnist/classifier.json 16:00:00 16G 128
+submit_metrics morphomnist hvae counterfactual_benchmark/methods/deepscm/configs/morphomnist/hvae.json counterfactual_benchmark/methods/deepscm/configs/morphomnist/classifier.json 16:00:00 16G 128 hvae_finetune
 submit_metrics morphomnist gan counterfactual_benchmark/methods/deepscm/configs/morphomnist/gan.json counterfactual_benchmark/methods/deepscm/configs/morphomnist/classifier.json 12:00:00 16G 128
 
 submit_metrics celeba_simple vae counterfactual_benchmark/methods/deepscm/configs/celeba/simple/vae.json counterfactual_benchmark/methods/deepscm/configs/celeba/simple/classifier.json 24:00:00 24G 64
-submit_metrics celeba_simple hvae counterfactual_benchmark/methods/deepscm/configs/celeba/simple/hvae.json counterfactual_benchmark/methods/deepscm/configs/celeba/simple/classifier.json 36:00:00 24G 64
+submit_metrics celeba_simple hvae counterfactual_benchmark/methods/deepscm/configs/celeba/simple/hvae.json counterfactual_benchmark/methods/deepscm/configs/celeba/simple/classifier.json 36:00:00 24G 64 hvae_finetune
 submit_metrics celeba_simple gan counterfactual_benchmark/methods/deepscm/configs/celeba/simple/gan.json counterfactual_benchmark/methods/deepscm/configs/celeba/simple/classifier.json 24:00:00 24G 64
 
 submit_metrics celeba_complex vae counterfactual_benchmark/methods/deepscm/configs/celeba/complex/vae.json counterfactual_benchmark/methods/deepscm/configs/celeba/complex/classifier.json 24:00:00 24G 64
-submit_metrics celeba_complex hvae counterfactual_benchmark/methods/deepscm/configs/celeba/complex/hvae.json counterfactual_benchmark/methods/deepscm/configs/celeba/complex/classifier.json 36:00:00 24G 64
+submit_metrics celeba_complex hvae counterfactual_benchmark/methods/deepscm/configs/celeba/complex/hvae.json counterfactual_benchmark/methods/deepscm/configs/celeba/complex/classifier.json 36:00:00 24G 64 hvae_finetune
 submit_metrics celeba_complex gan counterfactual_benchmark/methods/deepscm/configs/celeba/complex/gan.json counterfactual_benchmark/methods/deepscm/configs/celeba/complex/classifier.json 24:00:00 24G 64
